06a22fcd11458ad3e5d31f20ccfe2050d13ff5c1
bielalmd <bielalmd@gmail.com>
2023-11-15 20:51:57 -0300

    programação procedual

---
+++ app.js
@@ -0,0 +1,26 @@
+var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+var modificado = [];
+
+for (var i = 0; i < ingredientes.length; i++) {
+    var letraInicial = 
+        ingredientes[i].charAt(0).toUpperCase();
+    var restoTexto = 
+        ingredientes[i].slice(1);
+    var resultado = 
+        letraInicial + restoTexto;
+
+    modificado[i] = resultado
+}
+
+var ordenado = modificado.sort(function(a, b) {
+    return a.localeCompare(b)
+});
+
+console.log(ordenado);
+
+// var frase = 'Minha frase'
+
+// var posFrase = frase.charAt(6)
+
+// console.log(posFrase);
+
