e453d15218a928d79ef6f088466df499f8b3af0f
bielalmd <bielalmd@gmail.com>
2023-11-16 23:08:35 -0300

    objetos literais

---
+++ script/app.js
@@ -0,0 +1,23 @@
+import module from './module.js'
+import ingredients from './ingredients.js'
+import salads from './salads.js'
+
+console.log(ingredients);
+console.log(typeof ingredients);
+console.log(Array.isArray(ingredients));
+
+console.log(salads);
+console.log(typeof salads);
+console.log(salads[0]);
+
+
+// var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+// var resCap = module.capitalizar(ingredientes);
+// var resOrder = module.ordenar(resCap);
+// var resCaixaAlta = module.caixaAlta(ingredientes)
+
+// console.log(resCap);
+// console.log(resOrder);
+// console.log(resCaixaAlta);
+
+
